2100a3fdfe473a7afceadf0266cb5c9d6f94ee88
Christian F. Brinkmann <christian.brinkmann@rwth-aachen.de>
2026-07-14 10:26:21 +0200

    Clean up LD_LIBRARY_PATH from legacy GCC 11.3 / OpenMPI 4.1.4 in setup_env_claix23.sh

--- setup_env_claix23.sh
+++ setup_env_claix23.sh
@@ -27,3 +27,8 @@ module load Score-P/8.4-CUDA-12.4.0
 #module load cuDNN/9.7.0.66-CUDA-12.6.3
 
 #module load Score-P/9.0-CUDA-12.6.3
+
+# Clean LD_LIBRARY_PATH from conflicting GCC 11.3 / OpenMPI 4.1.4 entries
+if [[ -n "${LD_LIBRARY_PATH:-}" ]]; then
+    export LD_LIBRARY_PATH=$(echo "$LD_LIBRARY_PATH" | tr ':' '\n' | grep -v '11.3.0' | grep -v '2022a' | tr '\n' ':' | sed 's/:$//')
+fi
